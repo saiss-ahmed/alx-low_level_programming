a87a384f728c5da35af76f82eb4377e55da4290f
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-16 17:40:21 +0100

    4th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main (void) {
-		puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
-		}
+/**
+ * main - Entry point
+ *
+ * Description: Write a C program that prints the line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
