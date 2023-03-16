a4112b5665fceaf7e9187ad5c5a32f6a67769c0c
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-17 00:01:18 +0100

    doing the last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
