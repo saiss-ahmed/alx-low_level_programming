799fb8227f238d9a47d7f258d8458af80b2df88a
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-16 18:28:03 +0100

    6th task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Write a C program that prints the size of various types on the computer it
+ * printf:a command to output a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+	long c;
+	double f;
+	float d;
+	printf("size of char: %lu byte(s)\n", sizeof(b));
+	printf("size of an int: %lu byte(s)\n", sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(f));
+	printf("size of a float: %lu byte(s)\n", sizeof(d));
+	return (0);
+}
