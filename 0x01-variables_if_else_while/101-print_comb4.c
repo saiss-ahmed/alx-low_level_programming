97ae75225ab0f455cff5a4f03cf4423c7f459e50
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-17 19:35:04 +0100

    doing the 11th task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	do {
+		int j = 49;
+
+		do {
+			int k = 50;
+
+			do {
+
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+				{
+
+				} else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+			k++;
+			} while (k < 58);
+			j++;
+		} while (j < 57);
+		i++;
+	} while (i < 56);
+	putchar('\n');
+	return (0);
+}
