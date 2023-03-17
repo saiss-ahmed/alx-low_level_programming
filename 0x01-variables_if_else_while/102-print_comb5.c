1158f42d4c3b3fcf4f0ee1226b918240802678c8
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-17 22:15:13 +0100

    doing task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+		int n1 = 48;
+
+		do {
+			int n2 = 48;
+
+			do {
+				int n3 = 48;
+
+				do {
+					int n4 = 49;
+
+					do {
+					if (n3 >= n1 && n4 > n2)
+						{
+						putchar(n1);
+						putchar(n2);
+						putchar(' ');
+						putchar(n3);
+						putchar(n4);
+						putchar(',');
+						putchar(' ');
+						}
+					n4++;
+					} while (n4 < 58);
+				n3++;
+				} while (n3 < 58);
+			n2++;
+			} while (n2 < 57);
+		n1++;
+		} while (n1 < 58);
+}
