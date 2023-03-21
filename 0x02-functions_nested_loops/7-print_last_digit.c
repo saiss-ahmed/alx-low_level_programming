9aba8bf4cdb479c1fd8689b32348788dce1e1e48
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-21 19:30:39 +0000

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,11 @@ int print_last_digit(int x)
 {
 	if (x > 0)
 	{
-		_putchar(x % 10 + '0');
-		return (x % 10);
+		return (_putchar(x % 10 + '0'));
 	}
 	else
 	{
 		x = x * -1;
-		_putchar(x % 10 + '0');
-		return (x % 10);
+		return _putchar((x % 10 + '0'));
 	}
 }
