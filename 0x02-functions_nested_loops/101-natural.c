e099b7f966a932494dd71f86e6c06c2404a7581e
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-21 23:52:34 +0000

    task 14

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+
 /**
- *main - entry point
- *Return: always 0 (success)
+ * main - prints first 50 fibonacci numbers
+ * Return: 0 if succesfull
  */
+
 int main(void)
 {
-	int x;
-	int sum = 0;
+	long int a, b, c, d;
 
-	for (x = 0; x < 1024; x++)
+	b = 1;
+	c = 2;
+	d = 3;
+
+	for (a = 0; a < 50; a++)
 	{
-		if ((x % 3 == 0) || (x % 5 == 0))
-		{
-			sum += x;
-		}
+		if (a != 49)
+			printf("%ld, ", b);
+		else
+			printf("%ld\n", b);
+		d = c + d;
+		c = b + c;
+		b = d - c;
 	}
-	printf("%i\n", sum);
 	return (0);
 }
