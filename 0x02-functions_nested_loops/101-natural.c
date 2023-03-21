9648a40e3428db3e220fcbca4d612db40fc8ce95
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-21 23:33:09 +0000

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-/***/
-void sum_print(void);
+/**
+ *main - entry point
+ *Return: always 0 (success)
+ */
 int main(void)
-{
-	sum_print();
-
-}
-
-void sum_print(void)
 {
 	int x;
 	int sum = 0;
 
-	for ( x = 0; x < 1024; x++)
+	for (x = 0; x < 1024; x++)
 	{
 		if ((x % 3 == 0) || (x % 5 == 0))
 		{
@@ -20,4 +16,5 @@ void sum_print(void)
 		}
 	}
 	printf("%i\n", sum);
+	return (0);
 }
