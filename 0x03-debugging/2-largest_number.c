c9821d5692c0c4b9534d97858d841db5f4c891d0
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-22 11:12:58 +0000

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,31 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (a == b && b == c)
-	{
-		largest = a;
-	}
-	else if (a > b && b == c)
-	{
-		largest = a;
-	}
-	else if (b > a && a == c)
-	{
-		largest = b;
-	}
-	else if (c > a && a == b)
+	else 
 	{
 		largest = c;
 	}
-
-
 	return (largest);
 }
