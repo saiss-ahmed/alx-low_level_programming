19ce526b3b5c2a4ea2ef89922507e6a852d9dc73
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-22 08:57:05 +0000

    task 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <string.h>
 int _putchar(char c);
-int print_alphabet(void);
+void  print_alphabet(void);
 int print(char *array, int length);
 void print_alphabet_x10(void);
 int _islower(int c);
