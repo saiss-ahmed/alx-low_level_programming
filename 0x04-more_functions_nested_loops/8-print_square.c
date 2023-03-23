8eb955be8d0f493e4082e89b353c71e7fa22dfdf
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-23 15:27:33 +0000

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,16 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j  = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
 		_putchar('\n');
 	}
 
