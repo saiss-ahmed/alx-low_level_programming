17e18842627cacbb0a2d1495e1c062c746e23040
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-23 15:04:41 +0000

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,14 +14,11 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 			}
-		if (n != 0)
-		{
-			_putchar('\\');
-		}
-	_putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
 	}
-	if (n <0)
+	if (n < 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
