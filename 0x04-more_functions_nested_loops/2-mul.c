8a75c3f0ae05ca7d64d3cc92c3249714d302090e
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-23 12:59:51 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - a function that multiplies two integers
+ *@a: the first number
+ *@b: the second number
+ *Return: the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int tot = a * b;
+
+	return (tot);
+
+}
