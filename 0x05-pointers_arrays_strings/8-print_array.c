1a9ce60032ccdedcd6095563a8386c15b867abe0
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-27 23:59:08 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - a function that prints the arrays of ints
+ *@a: the array
+ *@n: the lenth of the array;
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	while (x < n - 1)
+	{
+		if (a[x] < 0)
+		{
+			a[x] = -a[x];
+			printf("-%d, ", a[x]);
+		}
+		else
+		{
+			printf("%d, ", a[x]);
+		}
+		x++;
+	}
+	if (a[x] < 0)
+	{
+		a[x] = -a[x];
+		printf("-%d", a[x]);
+	}
+	else
+	{
+	printf("%d", a[n]);
+	}
+	printf("\n");
+}
