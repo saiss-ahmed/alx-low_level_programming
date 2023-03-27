3138d20124fd30232c141d905fa80fe880aa8749
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-27 14:52:39 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen - print the length of a string
+ *@s: the first char of th string;
+ *Return: len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		*s++;
+	}
+
+	return (len);
+}
