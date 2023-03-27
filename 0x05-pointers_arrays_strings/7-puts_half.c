b20f6f761cebb5c0d8da31e342c78c25e3f2e574
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-27 22:35:55 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *puts_half - function that prints from the malf of th string
+ *@str: string from user
+ */
+
+void puts_half(char *str)
+{
+	int len	= _strlen(str);
+	int half = (len - 1) / 2;
+
+	while (str[half] != '\0')
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
+#include "main.h"
+/**
+ *_strlen - print the length of a string
+ *@s: the first char of th string;
+ *Return: len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
