593fef45e6b2a2fba3471029e3d43b8db81abc89
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-29 22:15:13 +0000

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/***/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+	for ( i = 0; i < (n/2); i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	
+	}
+
+}
