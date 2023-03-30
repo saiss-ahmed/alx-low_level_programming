605e72c43d0ff8a7b40e67677acac8919c6cef05
saiss-ahmed <ahmed2saiss@gmail.com>
2023-03-30 14:00:12 +0000

    task 8

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,44 +5,21 @@
  */
 void print_number(int n)
 {
-	int a = 0, b = 0;
-	int c = 1000000000;
-	int d = n;
+int divisor = 1;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		d = n;
-	}
-	while (d > 0)
-	{
-		a = d % 10;
-		b = b * 10 + a;
-		d = d / 10;
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		while (b > 0)
-		{
-			_putchar(b % 10 + '0');
-			b = b / 10;
-		}
-	}
-	if (n > 0)
-	{
-		while (c >= 10)
-		{
-			if (n % c == 0)
-			{
-				_putchar('0');
-			}
-		c = c / 10;
-		}
-	}
+if (n < 0)
+{
+_putchar('-');
+n = -n;
 }
 
+while (divisor <= n / 10)
+divisor *= 10;
+
+while (divisor > 0)
+{
+_putchar((n / divisor) % 10 + '0');
+divisor /= 10;
+}
+
+}
