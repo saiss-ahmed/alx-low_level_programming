50175d50815b3f43d11915e37f993c2b71dac7b4
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-03 12:51:10 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - a function that locates a character in a string.
+ *@s: the source sring
+ *@c: the letter that want to find
+ *Return: always s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (s);
+}
