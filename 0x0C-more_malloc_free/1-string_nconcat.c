b31b24a3c3fe55b40a56f7ceeab9556acde30a34
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-13 10:54:55 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,38 +9,29 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = _strlen(s1);
-	unsigned int len2 = _strlen(s2);
-	unsigned int i = 0, j = 0;
+
+	unsigned int len1;
+	unsigned int len2;
 	char *arr;
 
+	if (s1 == NULL)
+	{
+		s1 = (char *) malloc(1);
+		s1[0] = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = (char *) malloc(1);
+		s2[0] = '\0';
+	}
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	arr = (char *) malloc((len1 + len2 + 1) * sizeof(char));
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	for (; i < len1; i++)
-	{
-		arr[i] = s1[i];
-	}
-	if (n < len2)
-	{
-		for (; j <= n; j++)
-		{
-			arr[i] = s2[j];
-			i++;
-		}
-	}
-	else 
-	{
-		for (; j < len2; j++)
-		{
-			arr[i] = s2[j];
-			i++;
-		}
-	}
-	arr[i] = '\0';
-	return (arr);
+	return (concatinator(arr, s1, len1, s2, len2, n));
 }
 /**
   *_strlen - a function that find the length of a string
@@ -59,4 +50,41 @@ int _strlen(char *s)
 		}
 	}
 	return (i);
+}
+/**
+  *concatinator -a function conact two strings that has other paramethers
+  *@arr: the array that would concat everything
+  *@s1: the first string
+  *@s2: the second string
+  *@len1: the length for s1
+  *@len2: length for s2
+  *@n: look for the main functio to know
+  *Return: always an array
+  */
+char *concatinator(char *arr, char *s1, int len1, char *s2, int len2, int n)
+{
+	int i = 0, j = 0;
+
+	for (; i < len1; i++)
+	{
+		arr[i] = s1[i];
+	}
+	if (n < len2)
+	{
+		for (; j < n; j++)
+		{
+			arr[i] = s2[j];
+			i++;
+		}
+	}
+	else
+	{
+		for (; j < len2; j++)
+		{
+			arr[i] = s2[j];
+			i++;
+		}
+	}
+	arr[i] = '\0';
+	return (arr);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _strlen(char *s);
+char *concatinator(char *arr, char *s1, int len1, char *s2, int len2, int n);
