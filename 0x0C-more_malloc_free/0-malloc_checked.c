d65ab4c6b151e0e47a96693f5029b36fb59edf2e
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-13 09:37:22 +0000

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,21 @@
 #include "main.h"
-/***/
+/**
+  *malloc_checked - a function that allocate memory
+  *@b: the amount of bytes its gonna be located
+  *
+  *Return: a pointer
+  */
 
 void *malloc_checked(unsigned int b)
 {
 	int *arr;
-	
+
 	arr = (int *) malloc(b);
-	
+
 	if (arr == NULL)
 	{
 		return ("98");
 	}
-	
+
 	return (arr);
 }
