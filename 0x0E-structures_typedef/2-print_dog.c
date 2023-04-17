bb5e933d0313cd32584f667784cf63a8242ea372
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-17 18:35:33 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,12 +14,9 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("Name: (nil)");
+			d->name = "(nil)";
 		}
-		else
-		{
 		printf("Name: %s\n", d->name);
-		}
 		if (d->age < 0)
 		{
 			printf("Age: (nil)");
@@ -30,11 +27,8 @@ void print_dog(struct dog *d)
 		}
 		if (d->owner == NULL)
 		{
-			printf("Owner: (nil)");
+			d->owner = "(nil)";
 		}
-		else
-		{
 		printf("Owner: %s\n", d->owner);
-		}
 	}
 }
