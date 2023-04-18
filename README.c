25f675ba7d385c9ed4ef0d26b21db58e6d4b5785
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-18 21:59:03 +0000

    edit the README file

--- README.c
+++ README.c
@@ -1,1 +1,44 @@
-readme for c
+# alx low level programming README prject "not finished yet "
+# this was edited on 18/04/2023
+
+## this prject contain :
+
+* ### [0x00-hello_world](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x00-hello_world)
+* ### [0x01-variables_if_else_while](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x01-variables_if_else_while)
+* ### [0x02-functions_nested_loops](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x02-functions_nested_loops)
+* ### [0x03-debugging](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x03-debugging)
+* ### [0x04-more_functions_nested_loops](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x04-more_functions_nested_loops)
+* ### [0x05-pointers_arrays_strings](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x05-pointers_arrays_strings)
+* ### [0x06-pointers_arrays_strings](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x06-pointers_arrays_strings)
+* ### [0x07-pointers_arrays_strings](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x07-pointers_arrays_strings)
+* ### [0x08-recursion](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x08-recursion)
+* ### [0x09-static_libraries](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x09-static_libraries)
+* ### [0x0A-argc_argv](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x0A-argc_argv)
+* ### [0x0B-malloc_free](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x0B-malloc_free)
+* ### [0x0C-more_malloc_free](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x0C-more_malloc_free)
+* ### [0x0D-preprocessor](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x0D-preprocessor)
+* ### [0x0E-structures_typedef](https://github.com/saiss-ahmed/alx-low_level_programming/tree/main/0x0E-structures_typedef)
+## the purpose of this repository :
+this repo was made to lean C programming language, 
+### what is C ?
+C programming language is a widely used programming language that was originally developed by Dennis Ritchie at Bell Labs in 1972. It is a general-purpose, high-level language that is used to develop operating systems, database systems, compilers, and other system software. C is a compiled language, meaning that the source code is translated into machine code by a compiler before execution. It is known for its efficiency, portability, and low-level memory access, which makes it suitable for system-level programming. C has influenced many programming languages such as C++, Java, and Python.
+### here is some facts  about C 
+*  C was developed in the early 1970s by Dennis Ritchie at Bell Labs for the purpose of writing the UNIX operating system.
+
+* C is a general-purpose programming language that is used for system programming, embedded systems, and application programming.
+
+* C is a compiled language, which means that the code you write is converted into machine language by a compiler before it can be executed.
+
+* C is known for its efficiency, speed, and flexibility, making it popular in areas such as operating systems, network programming, and game development.
+
+* C is a low-level language, which means that it provides direct access to the computer's hardware, allowing for precise control over system resources.
+
+* C has influenced many other programming languages, including C++, Java, and Python.
+
+* C is still widely used today, particularly in system programming and embedded systems, despite the development of newer programming languages.
+
+* The C Standard Library provides a set of functions for common operations, such as string manipulation, memory allocation, and input/output.
+
+* C has a relatively simple syntax and is easy to learn, making it a good choice for beginners who want to learn programming.
+
+* C has had a significant impact on the development of modern computing and remains an important language for programmers and computer scientists today.
