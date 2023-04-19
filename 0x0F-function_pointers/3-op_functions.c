7738c71779c9cb964e83d5d9bcd49450f1d6c3bd
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-19 21:15:08 +0000

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,11 +47,7 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	int result;
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+
 	result = a / b;
 	return (result);
 }
@@ -64,11 +60,7 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	int result;
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+
 	result = a % b;
 	return (result);
 }
