85f3ef37f885986e41003bee13074701fd3760ec
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-19 13:55:11 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+  *print_name - a function that point to another function that prints a name
+  *@name: the par of name we are printing
+  *@f: the function we are pointing to
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+README FOR function_pointers
