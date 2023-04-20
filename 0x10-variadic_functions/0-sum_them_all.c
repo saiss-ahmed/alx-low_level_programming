b493af13b56f9f752ebc2a4a36fe92e2572151e6
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-20 12:58:14 +0000

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+  *sum_them_all - a function that prints the sum of all it pars
+  *@n: the counter
+  *Return: the sum
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	int x;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		sum += x;
+	}
+	va_end(args);
+	return (sum);
+}
