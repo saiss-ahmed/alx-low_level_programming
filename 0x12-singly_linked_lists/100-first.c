6cd62de5c4277dc4dd104c967e3be86519388d98
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-27 21:36:14 +0100

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_before_main - a function that print before the main fucntion
+  *Return: void
+  */
+void print_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
