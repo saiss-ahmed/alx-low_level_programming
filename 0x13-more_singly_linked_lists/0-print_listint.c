fb25c42de81407f9118e7f85cf0f2566b356fc94
saiss-ahmed <ahmed2saiss@gmail.com>
2023-04-29 10:34:36 +0100

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
 	}
 	return (count);
 }
