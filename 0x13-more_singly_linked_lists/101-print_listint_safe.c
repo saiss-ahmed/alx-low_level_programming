990b79c0a27f1f602c73e0c2a508d67da6e7b6ec
saiss-ahmed <ahmed2saiss@gmail.com>
2023-05-02 15:05:54 +0100

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (current->next >= current)
 		{
 			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			return (98);
 		}
 
 		current = current->next;
