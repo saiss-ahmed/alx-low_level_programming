48606ec081f9eac511dbddcff70fa3e49f4df81b
saiss-ahmed <ahmed2saiss@gmail.com>
2023-05-08 21:15:46 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,6 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, len = 0, check = 0;
+
 	if (!filename)
 		return (-1);
 	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
