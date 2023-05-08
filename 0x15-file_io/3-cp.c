181e24f648ea5e0be5b066cedd856df72a11539b
saiss-ahmed <ahmed2saiss@gmail.com>
2023-05-08 21:12:14 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,29 +1,88 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
-  * append_text_to_file - append to a file
-  * @filename: the name of the file
-  * @text_content: NULL terminated string to write to file
-  * Return: 1 on succes, -1 on failue
+ * copy_text_file - copies one file to the other (cp function)
+ * @f1: opened file1 (file to copy)
+ * @f2: opened file2 (new copy)
+ * @file1: name of file1
+ * @file2: name of file2
+ */
+
+void copy_text_file(int f1, int f2, char *file1, char *file2)
+{
+	ssize_t lenRead = 1, lenWrite = 1;
+	char buffer[1024];
+
+	while (lenRead)
+	{
+		lenRead = read(f1, buffer, sizeof(buffer));
+		if (lenRead == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+			close(f1);
+			close(f2);
+			exit(98);
+		}
+		if (!lenRead)
+			break;
+		lenWrite = write(f2, buffer, lenRead);
+		if (lenWrite == -1 || lenWrite != lenRead)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
+			close(f1);
+			close(f2);
+			exit(99);
+		}
+	}
+}
+
+/**
+  * main - copy a file into another file (cp command)
+  * @argc: number of arguments
+  * @argv: arguments
+  * Return: 0 if succesfull
   */
 
-int append_text_to_file(const char *filename, char *text_content)
+int main(int argc, char **argv)
 {
-	int file, len;
+	int f1, f2;
 
-	if (!filename)
-		return (-1);
-
-	file = open(filename, O_RDWR | O_APPEND);
-	if (file == -1)
-		return (-1);
-
-	if (text_content)
+	if (argc != 3)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
-		write(file, text_content, len);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	close(file);
-	return (1);
+
+	f1 = open(argv[1], O_RDONLY);
+	if (f1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	}
+	f2 = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
+	if (f2 == -1)
+		f2 = open(argv[2], O_TRUNC | O_WRONLY);
+	if (f2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(f1);
+		exit(99);
+	}
+
+	copy_text_file(f1, f2, argv[1], argv[2]);
+
+	if (close(f1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
+		close(f2);
+		exit(100);
+	}
+	if (close(f2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
+		exit(100);
+	}
+	return (0);
 }
