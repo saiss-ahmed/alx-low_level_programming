f713a1d21181fa71aa6c152cf0ba11fd9d41184b
saiss-ahmed <ahmed2saiss@gmail.com>
2023-05-08 21:15:14 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,14 +8,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, len = 0, check = 0;
-
 	if (!filename)
 		return (-1);
-
 	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
-
 	if (text_content)
 	{
 		for (len = 0; text_content[len]; len++)
@@ -25,6 +22,5 @@ int create_file(const char *filename, char *text_content)
 	close(file);
 	if (check != len)
 		return (-1);
-
 	return (1);
 }
